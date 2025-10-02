9be4df9f2416674463edde99d56cf0e5157b9645
alifaizyab8 <alifaizyab8@gmail.com>
2025-10-02 23:24:14 +0500

    understood sorting algorithm

--- Arrays/sort1.c
+++ Arrays/sort1.c
@@ -1,25 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    /*
-    How to sort an array in C?
-    loop and take first number and compare it with all other numbers
-    if any number is found smaller,assign that position to the first number(chosen)
-    and continue the loop
-
-    
-    */
-
-
-    int arr[10] = {34, 12, 5, 67, 1, 89, 23, 45, 78, 90};
-    int n = 10;
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nEnter the element number %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
     // Custom sorting algorithm: compare each element with the rest and swap if needed
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] < arr[j]) {
                 // Swap arr[i] and arr[j]
-                int temp = arr[i];
+                int swap = arr[i];
                 arr[i] = arr[j];
+                arr[j] = swap;
                 
             }
         }
