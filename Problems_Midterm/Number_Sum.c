a1b18426e3c243b5ac92295cc44ba1b99f918103
alifaizyab8 <alifaizyab8@gmail.com>
2025-10-04 12:32:59 +0500

    Number Manipulation

---
+++ Problems_Midterm/Number_Sum.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int rev_num(int);
+void Sep_Digit(int);
+int sum_Digit(int);
+int main()
+{
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("The reverse of %d is %d\n", n, rev_num(n));
+    Sep_Digit(rev_num(n));
+    printf("\nThe sum of digits is...%d", sum_Digit(n));
+}
+int sum_Digit(int n)
+{
+    int digit;
+    int sum = 0;
+    while (n > 0)
+    {
+        digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+void Sep_Digit(int n)
+{
+    int digit;
+    printf("The digits are:\n");
+    while (n > 0)
+    {
+        digit = n % 10;
+        printf(" %d,", digit);
+        n /= 10;
+    }
+}
+
+int rev_num(int n)
+{
+    int digit, rev = 0;
+    while (n > 0)
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+    return rev;
+}
